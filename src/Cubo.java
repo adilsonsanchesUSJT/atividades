c5dc0b6c69ea6cd009c06b8030c9772c4fcd555b
Adilson Sanches Junior <asjunior@bwg.com.br>
2020-03-11 15:29:52 -0300

    Exercicio 11/03 - Prof. Igor

---
+++ src/Cubo.java
@@ -0,0 +1,9 @@
+
+public class Cubo extends Poligono3D{
+
+	@Override
+	public double volume() {
+		return Math.pow(this.getBase(), 3);
+	}
+	
+}
---
+++ src/Piramide.java
@@ -0,0 +1,7 @@
+
+public class Piramide extends Poligono3D {
+	@Override
+	public double volume() {
+		return ((this.getBase() * this.getAltura()) * this.getProfundidade()) / 3;
+	}
+}
